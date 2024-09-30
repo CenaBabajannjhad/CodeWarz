ea1216831bf97aa507e8278eeb1366af725303f3
CenaBabajannjhad <cenababajannjhad@gmail.com>
2024-09-30 18:13:17 +0330

    i

---
+++ indexedCap.js
@@ -0,0 +1,4 @@
+//  capitalize("abcdef",[1,2,5]) = "aBCdeF"
+
+
+console.log(capitalize('cena' , [1,3]))
---
+++ sortWithAsortingArray.js
@@ -0,0 +1,8 @@
+const sort = (initArr , sortArr) => {
+    let result = [];
+    for(let i = 0 ; i < initArr.length ; i++){
+        
+    }
+}
+
+console.log(sort(['x', 'y', 'z'], [1, 2, 0]), ["z", "x", "y"], "For initialArray = ['x', 'y', 'z'], sortingArray = [1, 2, 0]");
